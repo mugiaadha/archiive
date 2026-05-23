f07f99306f52f57f232a1d12f4edae072fa12917
mugiaadha <mugia@investree.id>
2026-05-24 03:24:49 +0700

    pdate

--- ptsofcograha/api/api/src/main/java/aplikasicrud/api/repository/BarangRepository.java
+++
@@ -1,9 +0,0 @@
-package aplikasicrud.api.repository;
-
-import aplikasicrud.api.entities.Barang;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BarangRepository extends JpaRepository<Barang, String> {
-}
